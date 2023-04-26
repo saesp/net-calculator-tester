c820e55bf5b14da39804d02448ec31a00c0cb940
Salvatore Esposito <esposito.00@outlook.it>
2023-04-26 16:39:04 +0200

    added Calculator class with methods + Tests class with methods

--- net-calculator-tester/Program.cs
+++ net-calculator-tester/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Scopo di questa esercitazione è di implementare i metodi di una semplice calcolatrice e di scrivere una classe di test per verificare che funzionino come ci aspettiamo!
+//Creare una classe chiamata Calculator avente i seguenti metodi pubblici :
+//float Add(float num1, float num2)
+//float Subtract(float num1, float num2)
+//float Divide(float num1, float num2)
+//float Multiply(float numAdd1, float num2)
+
+namespace net_calculator_tester
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+    public class Calculator
+    {
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public static float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
+
+
+
--- net-calculator-testerTest/UnitTest1.cs
+++ net-calculator-testerTest/UnitTest1.cs
@@ -1,16 +1,37 @@
+using net_calculator_tester;
+
 namespace net_calculator_testerTest
 {
+    //Creare in seguito una classe di test che abbia 4 metodi, ognuno per verificare il corretto funzionamento di ogni metodo presente nella classe Calculator.
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void AddTest()
         {
+            Assert.IsTrue(Calculator.Add(2, 3) == 5);
+        }
+
+        [TestCase (6, 3, 3)]
+        [TestCase(10, 5, 5)]
+        [TestCase(3, 3, 1)]
+        public void SubtractTest(float a, float b, float result)
+        {
+            Assert.IsTrue(Calculator.Subtract(a, b) == result);
+
         }
 
         [Test]
-        public void Test1()
+        public void MultiplyTest()
         {
-            Assert.Pass();
+            Assert.IsTrue(Calculator.Multiply(3, 3) == 9);
+
+        }
+
+        [Test]
+        public void DivideTest()
+        {
+            Assert.IsTrue(Calculator.Divide(10, 2) == 5);
+
         }
     }
 }
